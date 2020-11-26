df7b172cf5df67cbc6168580543b00570df58e95
Xrhoys <yefeng@hotmail.fr>
2020-11-26 16:19:16 +0100

    refixed the font import

--- hooks/useCachedResources.ts
+++ hooks/useCachedResources.ts
@@ -20,6 +20,9 @@ export default function useCachedResources() {
         // Load fonts
         await Font.loadAsync({
           ...Ionicons.font,
+          ...MaterialIcons.font,
+          ...Entypo.font,
+          ...MaterialCommunityIcons.font,
           'space-mono': require('../assets/fonts/SpaceMono-Regular.ttf'),
         })
       } catch (e) {
