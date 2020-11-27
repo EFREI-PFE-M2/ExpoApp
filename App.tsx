38ef054aba4e467c99cdb181adb594b4a2a7a19d
Xrhoys <yefeng@hotmail.fr>
2020-11-27 22:40:02 +0100

    update files

--- App.tsx
+++ App.tsx
@@ -3,10 +3,25 @@ import React from 'react'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { Provider } from 'react-redux'
 import store from './store'
+import { Provider as PaperProvider, DefaultTheme } from 'react-native-paper'
 
 import useCachedResources from './hooks/useCachedResources'
 import useColorScheme from './hooks/useColorScheme'
 import Navigation from './navigation'
+import { Theme } from 'react-native-paper/lib/typescript/src/types'
+
+const theme: Theme = {
+  ...DefaultTheme,
+  roundness: 5,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#fff',
+    accent: '#194A4C',
+    placeholder: '#ffffff',
+    background: '#ffffff18',
+    text: '#fff',
+  },
+}
 
 export default function App() {
   const isLoadingComplete = useCachedResources()
@@ -17,10 +32,12 @@ export default function App() {
   } else {
     return (
       <Provider store={store}>
-        <SafeAreaProvider>
-          <Navigation colorScheme={colorScheme} />
-          <StatusBar />
-        </SafeAreaProvider>
+        <PaperProvider theme={theme}>
+          <SafeAreaProvider>
+            <Navigation colorScheme={colorScheme} />
+            <StatusBar />
+          </SafeAreaProvider>
+        </PaperProvider>
       </Provider>
     )
   }
