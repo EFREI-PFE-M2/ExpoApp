a92eae3bb74ba8d6c1f999a63971d227ef8c63a6
wil2106 <serviantwilliam@gmail.com>
2020-12-20 19:32:47 +0100

    race fakeapi cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,24 @@
 const functions = require('firebase-functions')
-
+const weekRaces = require('./weekRaces')
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 // Note: default deploy region is us-central-1, always specify to europe-west-1
+
 
 exports.helloWorld = functions
   .region('europe-west1')
   .https.onRequest((request, response) => {
     functions.logger.info('Hello logs!', { structuredData: true })
     response.send('Hello from Firebase!')
+  })
+  
+exports.races = functions
+  .region('europe-west1')
+  .https.onRequest((request, response) => {
+    let dateParameter = request.query.date
+    let date = new Date(dateParameter)
+    let dayID = date.getDay()
+    let dayRaces = races.filter((race)=>race.weekDayID === dayID)
+    .map((race=> ({...race, date: `${dateParameter} ${race.hour}` })))
+    response.send(dayRaces)
   })
