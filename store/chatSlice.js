f0c46c4ec7cd9f883f1bfc7a3e45f42ea4847c7a
Xrhoys <yefeng@hotmail.fr>
2020-12-26 16:24:36 +0100

    added redux store example data

--- store/chatSlice.js
+++ store/chatSlice.js
@@ -3,7 +3,83 @@ import { createSlice } from '@reduxjs/toolkit'
 export const chatSlice = createSlice({
   name: 'chat',
   initialState: {
-    privateConversations: [],
+    privateConversations: {
+      1: {
+        senderID: 'KfOdJZSnByX1iQNZdy5cyDVLSh03',
+        senderDisplayName: 'Test user 1',
+        senderPhotoURL:
+          'https://upload.wikimedia.org/wikipedia/commons/5/56/Donald_Trump_official_portrait.jpg',
+        receiverID: 'MD3IFJBvLQbKkR3Z8g2BEGJ2Lht2',
+        receiverDisplayName: 'Test user 2',
+        receivePhotoURL:
+          'https://media.npr.org/assets/img/2020/11/01/gettyimages-1256154622_custom-75dab75fd97ed1b3b9a761385d2c33284789bc3b-s800-c85.jpg',
+        messages: [
+          {
+            type: 'text',
+            createdAt: new Date().toLocaleString(),
+            displayName: 'Test user 1',
+            photoURL:
+              'https://upload.wikimedia.org/wikipedia/commons/5/56/Donald_Trump_official_portrait.jpg',
+            uid: 'KfOdJZSnByX1iQNZdy5cyDVLSh03',
+            text: 'Lorem ipsum [...]',
+          },
+          {
+            type: 'text',
+            createdAt: new Date().toLocaleString(),
+            displayName: 'Test user 2',
+            photoURL:
+              'https://media.npr.org/assets/img/2020/11/01/gettyimages-1256154622_custom-75dab75fd97ed1b3b9a761385d2c33284789bc3b-s800-c85.jpg',
+            uid: 'MD3IFJBvLQbKkR3Z8g2BEGJ2Lht2',
+            text: 'Lorem ipsum [...]',
+          },
+          {
+            type: 'text',
+            createdAt: new Date().toLocaleString(),
+            displayName: 'Test user 1',
+            photoURL:
+              'https://upload.wikimedia.org/wikipedia/commons/5/56/Donald_Trump_official_portrait.jpg',
+            uid: 'KfOdJZSnByX1iQNZdy5cyDVLSh03',
+            text: 'Lorem ipsum [...]',
+          },
+          {
+            type: 'text',
+            createdAt: new Date().toLocaleString(),
+            displayName: 'Test user 1',
+            photoURL:
+              'https://upload.wikimedia.org/wikipedia/commons/5/56/Donald_Trump_official_portrait.jpg',
+            uid: 'KfOdJZSnByX1iQNZdy5cyDVLSh03',
+            text: 'Lorem ipsum [...]',
+          },
+          {
+            type: 'text',
+            createdAt: new Date().toLocaleString(),
+            displayName: 'Test user 2',
+            photoURL:
+              'https://media.npr.org/assets/img/2020/11/01/gettyimages-1256154622_custom-75dab75fd97ed1b3b9a761385d2c33284789bc3b-s800-c85.jpg',
+            uid: 'MD3IFJBvLQbKkR3Z8g2BEGJ2Lht2',
+            text: 'Lorem ipsum [...]',
+          },
+          {
+            type: 'text',
+            createdAt: new Date().toLocaleString(),
+            displayName: 'Test user 2',
+            photoURL:
+              'https://media.npr.org/assets/img/2020/11/01/gettyimages-1256154622_custom-75dab75fd97ed1b3b9a761385d2c33284789bc3b-s800-c85.jpg',
+            uid: 'MD3IFJBvLQbKkR3Z8g2BEGJ2Lht2',
+            text: 'Lorem ipsum [...]',
+          },
+          {
+            type: 'text',
+            createdAt: new Date().toLocaleString(),
+            displayName: 'Test user 1',
+            photoURL:
+              'https://upload.wikimedia.org/wikipedia/commons/5/56/Donald_Trump_official_portrait.jpg',
+            uid: 'KfOdJZSnByX1iQNZdy5cyDVLSh03',
+            text: 'Lorem ipsum [...]',
+          },
+        ],
+      },
+    },
     groupConversations: [],
     searchedUsers: [],
   },
