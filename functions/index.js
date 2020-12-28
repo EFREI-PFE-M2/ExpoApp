d7d163ed9d66ea2a26be48946f7481bf7416dea4
Xrhoys <yefeng@hotmail.fr>
2020-12-28 18:21:23 +0100

    firestore functions trigger
    createConversation + createMessageTriggers

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
+const { firestore } = require('firebase-admin')
 
 admin.initializeApp()
 
@@ -26,4 +27,53 @@ exports.onCreateUser = functions
       .doc(uid)
       .set({ email, createdAt: admin.firestore.Timestamp.fromDate(new Date()) })
       .catch(console.error)
+  })
+
+exports.onCreateConversation = functions
+  .region('europe-west1')
+  .firestore.document('PrivateConversation/{id}')
+  .onCreate((snapshot, context) => {
+    const { users } = snapshot.data()
+
+    const fetchAsync = async () => {
+      try {
+        const [user0, user1] = await Promise.all([
+          db.collection('Users').doc(users[0]).get(),
+          db.collection('Users').doc(users[1]).get(),
+        ])
+
+        return { user0, user1 }
+      } catch (err) {
+        console.error(err)
+      }
+    }
+
+    return fetchAsync()
+      .then(({ user0, user1 }) => {
+        const data0 = user0.data()
+        const data1 = user1.data()
+
+        return snapshot.ref.update({
+          senderID: user0.id,
+          receiverID: user1.id,
+          senderDisplayName: data0.displayName || null,
+          receiverDisplayName: data1.displayName || null,
+          senderPhotoURL: data0.photoURL || null,
+          receiverPhotoURL: data1.photoURL || null,
+          createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+        })
+      })
+      .catch(console.error)
+  })
+
+// TODO: onSendingMessage
+exports.onCreateMessage = functions
+  .region('europe-west1')
+  .firestore.document(
+    'PrivateConversation/{conversationID}/messages/{messageID}'
+  )
+  .onCreate((snapshot, context) => {
+    return snapshot.ref.update({
+      createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+    })
   })
