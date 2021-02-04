11b634052c9e83eebd6cd68e3aac8564e119e824
Xrhoys <yefeng@hotmail.fr>
2021-02-04 16:46:58 +0100

    added usercard redirection

--- components/UserCard_Small.tsx
+++ components/UserCard_Small.tsx
@@ -3,14 +3,15 @@ import { Image, StyleSheet } from 'react-native'
 import { TouchableRipple } from 'react-native-paper'
 import { useSelector } from 'react-redux'
 import Navigation from '../navigation'
+import { userSlice } from '../store/userSlice'
 import { View, Text } from './Themed'
 
 export default function UserCard({ user, navigate }) {
   const { photoURL, displayName } = useSelector(({ search }) => search.search.searchedUsers[user])
 
-  const onPress = () => navigate('Home_Profile', {})
+  const onPress = () => navigate('Home_Profile', { id: userSlice })
   return (
-    <TouchableRipple style={styles.container} onPress={}>
+    <TouchableRipple style={styles.container} onPress={onPress}>
       <Image source={{ uri: photoURL || '' }} style={styles.image} />
       <Text style={styles.title}>{displayName}</Text>
     </TouchableRipple>
