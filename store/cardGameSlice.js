fec9293e881bb5dba1e2516e706caa5e8e129142
Drilegrand <adrien.demagny.com@gmail.com>
2021-02-10 13:37:31 +0100

    functions getDeck and searchUser working

--- store/cardGameSlice.js
+++ store/cardGameSlice.js
@@ -6,6 +6,7 @@ export const cardGameSlice = createSlice({
   initialState: {
     ongoingGames: [],
     searchedUsers: [],
+    deck: [],
   },
   reducers: {
     addSearchedUser:(state, action)=>{
@@ -13,6 +14,12 @@ export const cardGameSlice = createSlice({
     },
     clearSearchedUser:(state)=>{
       state.searchedUsers=[]
+    },
+    addDeck:(state, action)=>{
+      state.deck.push(action.payload)
+    },
+    clearDeck:(state)=>{
+      state.deck=[]
     },
   },
 })
@@ -52,24 +59,39 @@ game object format: {
 export const {
   addSearchedUser,
   clearSearchedUser,
+  addDeck,
+  clearDeck
 } = cardGameSlice.actions
 
 // thunks
-export const getDeck = async(userId) =>{
-  await firestore.collection('CardsDeck').where('userId','==',userId).get()
+export const getDeck = (userId) => async(dispatch) =>{
+  await dispatch(clearDeck())
+  const idDeck = await firestore.collection('CardsDeck').where('userId','==',userId).get()
+  for (var i=0; i<idDeck.docs.length; i++){
+    cards = await idDeck.docs[i].get('cards')
+    for (const idCard of cards){
+      card = await firestore.collection('Cards').doc(idCard).get()
+      await dispatch(addDeck(card.data()))
+    }
+  }
 }
 
 export const searchUser = (userName) => async(dispatch) =>{
   await dispatch(clearSearchedUser())
-  const searchedUser = await firestore.collection('Users').orderBy('displayName').startAt(username).endAt(username+'\uf8ff').get()
+  const searchedUser = await firestore.collection('Users').orderBy('displayName').startAt(userName).endAt(userName+'\uf8ff').get()
   searchedUser.forEach(async (element) => {
-    await dispatch(addSearchedUser(element))
+    await dispatch(addSearchedUser({
+      userID: element.id,
+      username: element.get('displayName'),
+      profilePicture: element.get('photoURL')
+    }))
   });
 }
 
 
 // selectors
 export const selectUserResults = ({ cardGame }) => cardGame.searchedUsers
+export const selectDeck = ({ cardGame }) => cardGame.deck
 
 
 export const cardGameReducer = cardGameSlice.reducer
