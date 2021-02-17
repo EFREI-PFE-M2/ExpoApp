1d5206d7b01873e33980d8b09e4a455630d52aa6
Yefan Guo <guo.yefan@hotmail.fr>
2021-02-17 12:57:05 +0100

    add notification store

--- store/notificationSlice.js
+++ store/notificationSlice.js
@@ -4,47 +4,45 @@ import { FirebaseFirestore as firestore } from '../firebase'
 export const notificationSlice = createSlice({
   name: 'notification',
   initialState: {
-    notifications: {},
+    notifications: [],
   },
   reducers: {
     updateNotifications: (state, action) => {
-      const { id, notification } = action.payload
-      state.notifications[id] = notification
+      state.notifications = action.payload
     },
   },
 })
 
 /**
- notification
- --- type: follow/like/comment
- --- datatime
- --- userDetails:
-    --- uid
-    --- displayName
-    --- photoURL
- --- listeners: array of uid
- */
-
-/**
  * @param {Object} notification
  */
 export const createNotification = (notification) => async (dispatch) => {
-  if (!notification) return console.log('notif null')
-
-  const user = notification.userDetails
-  const listeners = notification.listeners
-
-  if (!user || !listeners) return console.log('user or listeners null')
-
-  const ref = firestore.collection('Users')
-
-  if (listeners.length > 1) {
-    await ref.get().then((doc) => console.log(doc.id))
-  } else {
-    if (listeners.length == 1) {
-      await ref.doc(listeners[0]).collection('Notifications').add(notification)
-    }
+  try {
+    const userID = notification.userID
+    const ref = firestore.collection(`Users/${userID}/Notifications`)
+    await ref.add(notification)
+  } catch (err) {
+    console.error(err)
   }
 }
+
+/**
+ * @param {String} userID
+ */
+export const getNotifications = (userID) => async (dispatch, getState) => {
+  try {
+    const { notification } = getState()
+    const ref = firestore.collection(`Users/${userID}/Notifications`)
+
+    const snapshot = await ref.orderBy('datetime', 'desc').limit(10).get()
+
+    const notifications = snapshot.docs.map((doc) => doc.data())
+    await dispatch(updateNotifications(notifications))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export const selectNotifications = (state) => state.notification.notifications
 
 export const notificationReducer = notificationSlice.reducer
